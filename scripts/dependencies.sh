911b4abcc8a9c47cdceb7c28d43bc13b44dfb85c
Lyon Till <158992+ljtill@users.noreply.github.com>
2024-02-16 17:44:40 +0000

    feat: add wasi-sdk dependency (#18)

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Path to the dotnet executable
 DOTNET=/usr/share/dotnet/dotnet
 
@@ -16,5 +18,16 @@ if ! $DOTNET workload install wasi-experimental; then
     echo "Failed to install wasi-experimental workload. Exiting." >&2
     exit 1
 fi
+
+# Install wasi-sdk
+echo "Installing wasi-sdk..."
+WASI_SDK_URL="https://github.com/webassembly/wasi-sdk/releases/download/${WASI_VERSION}/${WASI_VERSION}.0-linux.tar.gz"
+WASI_SDK_TMP="/tmp/${WASI_VERSION}.0-linux.tar.gz"
+WASI_SDK_DIR="/usr/local/lib/${WASI_VERSION}.0"
+
+curl -Lo "${WASI_SDK_TMP}" "${WASI_SDK_URL}"
+tar -xzf "${WASI_SDK_TMP}" -C /tmp/
+rm "${WASI_SDK_TMP}"
+mv "/tmp/${WASI_VERSION}.0" "${WASI_SDK_DIR}"
 
 echo "Script execution completed successfully."
